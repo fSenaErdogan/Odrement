6a1b004c8832fac106bb424ef21e7ddff6a3675d
fSenaErdogan <faseerd@gmail.com>
2023-10-29 14:56:18 +0300

    edit:buttons

--- src/Components/Buttons/Pastel/Button14/index.jsx
+++ src/Components/Buttons/Pastel/Button14/index.jsx
@@ -2,10 +2,10 @@
 const PButton14 = ({ children }) => {
 
     return (
-        <button className="bg-purple-200 px-5 py-2 rounded-full text-md">
+        <button className="hover:bg-opacity-100 text-purple-700 border border-purple-600 
+        bg-purple-200 bg-opacity-20 px-5 py-2 rounded-full text-md transition-all duration-500">
             {children}
         </button>
-
     )
 }
 
--- src/Components/Buttons/Pastel/index.jsx
+++ src/Components/Buttons/Pastel/index.jsx
@@ -1,12 +1,12 @@
-import PButton1 from "./Button1" //BUTONLAR
-import PButton2 from "./Button2"
-import PButton3 from "./Button3"
-import PButton4 from "./Button4"
-import PButton5 from "./Button5"
-import PButton6 from "./Button6"
-import PButton7 from "./Button7"
-import PButton8 from "./Button8"
-import PButton9 from "./Button9"
+import PButton1 from "./Button01" //BUTONLAR
+import PButton2 from "./Button02"
+import PButton3 from "./Button03"
+import PButton4 from "./Button04"
+import PButton5 from "./Button05"
+import PButton6 from "./Button06"
+import PButton7 from "./Button07"
+import PButton8 from "./Button08"
+import PButton9 from "./Button09"
 import PButton10 from "./Button10"
 import PButton11 from "./Button11"
 import PButton12 from "./Button12"
@@ -25,39 +25,39 @@ const Pastel = () => {
         <>
             <div className="pastel bg-white p-5 rounded-md items-center gap-5 flex flex-wrap text-black/70">
                 <PButton1>
-                    button 1
+                    button 01
                 </PButton1>
 
                 <PButton2>
-                    button 2
+                    button 02
                 </PButton2>
 
                 <PButton3>
-                    button 3
+                    button 03
                 </PButton3>
 
                 <PButton4>
-                    button 4
+                    button 04
                 </PButton4>
 
                 <PButton5>
-                    button 5
+                    button 05
                 </PButton5>
 
                 <PButton6>
-                    button 6
+                    button 06
                 </PButton6>
 
                 <PButton7>
-                    button 7
+                    button 07
                 </PButton7>
 
                 <PButton8>
-                    button 8
+                    button 08
                 </PButton8>
 
                 <PButton9>
-                    button 9
+                    button 09
                 </PButton9>
 
                 <PButton10>
@@ -71,7 +71,7 @@ const Pastel = () => {
                 <PButton12>
                     button 12
                 </PButton12>
-                
+
                 <PButton13>
                     button 13
                 </PButton13>
@@ -83,7 +83,7 @@ const Pastel = () => {
                 <PButton15>
                     button 15
                 </PButton15>
-                
+
                 <PButton16>
                     button 16
                 </PButton16>
@@ -103,7 +103,7 @@ const Pastel = () => {
                 <PButton20>
                     button 20
                 </PButton20>
-                
+
             </div>
         </>
     )
